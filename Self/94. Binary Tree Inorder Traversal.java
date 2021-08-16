0eb0995e30d23b3c912d4c7d3e76a420fb037cde
BellaXiao <286773696@qq.com>
2021-08-15 21:14:42 -0500

    21/8/16 Tree Review easy 94

--- Self/94. Binary Tree Inorder Traversal.java
+++ Self/94. Binary Tree Inorder Traversal.java
@@ -122,6 +122,48 @@ class Solution {
 
 
 
+// self review - iterative way
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+        if (root == null) {return res;}
+        
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while(cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            
+            cur = stack.pop();
+            res.add(cur.val);
+            cur = cur.right;
+        }
+        return res;
+        
+        
+    }
+}
+
+
+
+
 
 
 
