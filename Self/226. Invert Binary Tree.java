81621126a0c81071f2396270943aded4798ce1a7
BellaXiao <286773696@qq.com>
2021-08-17 08:53:25 -0500

    21/8/17 Tree Topic Basic review

--- Self/226. Invert Binary Tree.java
+++ Self/226. Invert Binary Tree.java
@@ -26,3 +26,24 @@ class Solution {
         return root;
     }
 }
+
+// self review - iterative way
+// in fact any preorder way can do it, BFS also works
+class Solution {
+    // can also do iteratively
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {return null;}
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            TreeNode temp = cur.left;
+            cur.left = cur.right;
+            cur.right = temp;
+            if (cur.right != null) {stack.push(cur.right);}
+            if (cur.left != null) {stack.push(cur.left);}
+        }
+        
+        return root;
+    }
+}
