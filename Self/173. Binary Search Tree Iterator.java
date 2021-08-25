a21eb86c149dd08c6e213e2027ce559248b5467e
BellaXiao <286773696@qq.com>
2021-08-25 09:09:24 -0500

    21/8/25 Tree New Medium 173

---
+++ Self/173. Binary Search Tree Iterator.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// self first try - optimal answer: next() and hasNext has average O(1) time and O(h) space
+class BSTIterator {
+    private TreeNode lastPop;
+    private Stack<TreeNode> stack;
+    
+    
+    public BSTIterator(TreeNode root) {
+        lastPop = new TreeNode(-1);
+        lastPop.right = root;
+        stack = new Stack<>();
+    }
+    
+    public int next() {
+        // don't forget to add lastPop.right if it's not null
+        if (stack.isEmpty() || lastPop.right != null) {
+            TreeNode p = lastPop.right;
+            while (p != null) {
+                stack.push(p);
+                p = p.left;
+            }
+        }
+        lastPop = stack.pop();
+        return lastPop.val;
+    }
+    
+    public boolean hasNext() {
+        return (!stack.isEmpty() || lastPop.right != null);
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
+
+
+/*
+When analyzing amortized time complexities, I find it easiest to reason that each node gets pushed and popped exactly once in next() when iterating over all N nodes.
+That comes out to 2N * O(1) over N calls to next(), making it O(1) on average, or O(1) amortized.
+*/
+// similar idea: https://leetcode.com/problems/binary-search-tree-iterator/discuss/52526/Ideal-Solution-using-Stack-(Java)
+
+
+
+
+
+
+
+
+
