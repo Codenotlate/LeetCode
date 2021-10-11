3bb1a43c1cbcd0120a542fe808a785d32f74e75b
Codenotlate <286773696@qq.com>
2021-10-10 22:47:38 -0500

    21/10/10 DP Review Medium 213

--- Self/213. House Robber II.java
+++ Self/213. House Robber II.java
@@ -26,3 +26,29 @@ class Solution {
 
 // thinking process
 // https://leetcode.com/problems/house-robber-ii/discuss/227366/Thinking-process-from-easy-question-to-harder-question-within-the-same-question-set
+
+
+
+// Phase3 self
+// similar as 198 phase3, move forward, above method is moving backwards
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        // note pay attention to n=1 special case
+        if (n == 1) {return nums[0];}
+        int res1 = getRes(nums, 0, n - 2);
+        int res2 = getRes(nums, 1, n - 1);
+        return Math.max(res1, res2);
+    }
+    
+    private int getRes(int[] nums, int start, int end) {
+        int dp_i0 = 0;
+        int dp_i1 = nums[start];
+        for (int i = start + 1; i <= end; i++) {
+            int temp = dp_i0;
+            dp_i0 = Math.max(dp_i0, dp_i1);
+            dp_i1 = temp + nums[i];
+        }
+        return Math.max(dp_i1, dp_i0);
+    }
+}
