0e4be4d9b739b891d12150d53b50a62696b1de5f
Codenotlate <286773696@qq.com>
2021-10-14 10:04:31 -0500

    21/10/14 DP Review Medium 91

--- Self/91. Decode Ways.java
+++ Self/91. Decode Ways.java
@@ -19,3 +19,61 @@ class Solution {
         return dp[n];
     }
 }
+
+
+
+// Phase3 self
+class Solution {
+    // M1: Top down: dfs + memo  
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] memo = new int[n];
+        Arrays.fill(memo, -1);
+        Set<String> codeSet = new HashSet<>();
+        for (int i = 1; i <= 26; i++) {
+            codeSet.add(Integer.toString(i));
+        }
+        return dfs(s, 0, memo, codeSet);
+    }
+    
+    private int dfs(String s, int curIdx, int[] memo, Set<String> codeSet) {
+        if(curIdx >= s.length()) {
+            return 1;
+        }
+        if (memo[curIdx] != -1) {return memo[curIdx];}
+        
+        memo[curIdx] = 0;
+        for (int i = 1; i <= 2 && curIdx + i <= s.length(); i++) {
+            String sub = s.substring(curIdx, curIdx + i);
+            if (codeSet.contains(sub)) {
+                memo[curIdx] += dfs(s, curIdx + i, memo, codeSet);
+            }
+        }
+        return memo[curIdx];
+        
+    }
+} 
+
+
+class Solution {
+    // M2: Bottom-up: dp 
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        Set<String> codeSet = new HashSet<>();
+        for (int i = 1; i <= 26; i++) {
+            codeSet.add(Integer.toString(i));
+        }
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <=2; k++) {
+                if(i - k >= 0 && codeSet.contains(s.substring(i - k, i))) {
+                    dp[i] += dp[i - k];
+                }
+            }
+        }
+        
+        
+        return dp[n];
+    }
+} 
