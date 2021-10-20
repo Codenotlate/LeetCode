1fd917e396d5b5f3998b0ad64a142a47d8b76c51
Codenotlate <286773696@qq.com>
2021-10-20 10:23:09 -0500

    21/10/20 LinkedList Review Medium 2

--- Self/2. Add Two Numbers.java
+++ Self/2. Add Two Numbers.java
@@ -30,3 +30,30 @@ class Solution {
         return newHead.next;
     }
 }
+
+
+// Phase3 self
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode resHead = new ListNode(-1, null);
+        ListNode res = resHead;
+        ListNode p1 = l1;
+        ListNode p2= l2;
+        int add = 0;
+        while (p1 != null || p2 != null) {
+            int val1 = p1 == null? 0 : p1.val;
+            int val2 = p2 == null? 0 : p2.val;
+            int sum = val1 + val2 + add;
+            add = sum /10;
+            res.next = new ListNode(sum % 10, null);
+            res = res.next;
+            p1 = p1 == null? p1 : p1.next;
+            p2 = p2 == null? p2 : p2.next;      
+        }
+        if (add != 0) {
+            res.next = new ListNode(add, null);
+        }
+        return resHead.next;
+    }
+}
