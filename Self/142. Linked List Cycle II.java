e25c2d1fc1cab3f5ed3e5ed9d89fb3e28ec57bd0
Codenotlate <286773696@qq.com>
2021-10-22 15:35:45 -0500

    21/10/22 LinkedList Review Medium 142

--- Self/142. Linked List Cycle II.java
+++ Self/142. Linked List Cycle II.java
@@ -66,6 +66,28 @@ public class Solution {
 
 
 
+// Phase 3 self
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        // time O(n), space O(1)
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast!= null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {break;}
+        }
+        if (fast == null || fast.next == null) {return null;}
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
+
+
 
 // another way of writing method 2 (more precise)
 public class Solution {
