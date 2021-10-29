24351cf35edcd59c2d37d6be86a59b9f5b19424a
Codenotlate <286773696@qq.com>
2021-10-29 09:08:43 -0500

    21/10/29 Binary Search Review Medium 33

--- Self/33. Search in Rotated Sorted Array.java
+++ Self/33. Search in Rotated Sorted Array.java
@@ -29,3 +29,37 @@ class Solution {
         return -1;
     }
 }
+  
+
+
+
+
+// Phase3 self
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {return mid;}
+            // pay attention the equal sign here
+            if (nums[mid] >= nums[start]) {
+                if (target > nums[mid] || target < nums[start]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                if (target < nums[mid] || target >= nums[start]) { // pay attention the equal sign here
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
+
+// The key is to draw out the 4 graphs in the goodnote and categories them
