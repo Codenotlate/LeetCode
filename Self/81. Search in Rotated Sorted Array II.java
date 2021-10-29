94fc17c0dbeaf940fe5bc9063f445c8e24026306
Codenotlate <286773696@qq.com>
2021-10-29 09:37:16 -0500

    21/10/29 BS Review Medium 81

--- Self/81. Search in Rotated Sorted Array II.java
+++ Self/81. Search in Rotated Sorted Array II.java
@@ -81,3 +81,33 @@ public boolean search(int[] nums, int target) {
 
 	return false;
 }
+
+
+
+// Phase 3 self
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+            if (nums[mid] == target) {return true;}
+            if (nums[start] < nums[mid]) {
+                if (nums[start] <= target && target < nums[mid]) {
+                    end = mid -1;
+                } else {
+                    start = mid + 1;
+                }
+            } else if (nums[start] > nums[mid]) {
+                if (nums[mid] < target && nums[end] >= target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else {// can only remove nums[start] if nums[start] == nums[mid]
+                start += 1;
+            }
+        }
+        return false;
+    }
+}
