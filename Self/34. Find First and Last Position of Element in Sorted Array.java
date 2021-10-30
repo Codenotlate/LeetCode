1edeeb05ec5df1bfd280b87202dc4f744cee1392
Codenotlate <286773696@qq.com>
2021-10-30 12:30:36 -0500

    21/10/30 BS Review Medium 34

--- Self/34. Find First and Last Position of Element in Sorted Array.java
+++ Self/34. Find First and Last Position of Element in Sorted Array.java
@@ -133,3 +133,53 @@ class Solution {
         return nums[s] >= target? s : -1;
     }
 }
+
+
+
+
+
+// Phase3 self 2.0 (another way to find first and last, remember to use two elements case to check whether a infinite loop)
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = searchFirst(nums, target);
+        result[1] = searchLast(nums, target);
+        return result;
+    }
+    
+    private int searchFirst(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        if (end < 0) {return -1;}
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                end = mid;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return nums[start] == target ? start : -1;
+    }
+    
+    private int searchLast(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        if (end < 0) {return -1;}
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                if (nums[mid + 1] > target) {return mid;}
+                start = mid + 1;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return nums[start] == target ? start : -1;
+    }
+    
+}
