bb44a5f1d884687a3429a1f511d0480411bff0e2
Codenotlate <286773696@qq.com>
2021-10-30 12:01:54 -0500

    21/10/30 BS Review Medium 162

--- Self/162. Find Peak Element.java
+++ Self/162. Find Peak Element.java
@@ -29,3 +29,23 @@ Binary search works since :
 2) peak definition can be checked with 2 indices adjacent to the mid value. (nums[i] != nums[i + 1] for any i)
 3) nums[-1] = nums[n] = -inf
 */
+
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            long left = mid - 1 < start ? -Long.MIN_VALUE : nums[mid - 1];
+            long right = mid + 1 > end ? -Long.MIN_VALUE : nums[mid + 1];
+            int middle = nums[mid];
+            if (middle > left && middle > right) {return mid;}
+            if (left < middle && middle < right) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }          
+        }
+        return start;
+    }
+}
