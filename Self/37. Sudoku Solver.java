33123b21e7223ddc4b1b3b4f284d418fc135bd18
Codenotlate <286773696@qq.com>
2021-11-21 17:44:08 -0600

    21/11/21 DFS+backtracking Review Hard 37

--- Self/37. Sudoku Solver.java
+++ Self/37. Sudoku Solver.java
@@ -36,3 +36,81 @@ class Solution {
     	return true;
     }
 }
+
+
+
+class Solution {
+    // phase3 self: DFS + backtracking
+    // similar as 36, use arrays cols\rows\blocks to track numbers used
+    // time O(9^m) where m represents the number of unfilled cells in board
+    public void solveSudoku(char[][] board) {
+        int[][] cols = new int[9][9];
+        int[][] rows = new int[9][9];
+        int[][] blocks = new int[9][9];
+        // update cols\rows\blocks with already filled cells
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    cols[j][num] = 1;
+                    rows[i][num] = 1;
+                    blocks[i / 3 + j / 3 * 3][num] = 1;
+                }
+            }
+        }
+        
+        dfs(board, 0, 0, cols, rows, blocks);
+    }
+    
+    private boolean dfs(char[][] board, int i, int j, int[][] cols, int[][] rows, int[][] blocks) {
+        // end condition
+        if (i*9 + j >= 81) {return true;}
+        if (board[i][j] != '.') { // cell is filled already
+            if (j < 8) {return dfs(board, i, j+1, cols, rows, blocks);}
+            else {return dfs(board, i+1, 0, cols, rows, blocks);}
+        } else { // unfilled cell
+            for (char k = '1'; k <= '9'; k++) {
+                if (isValid(i, j, k-'1', cols, rows, blocks)) {
+                    // fill and update
+                    board[i][j] = k;
+                    cols[j][k-'1'] = 1;
+                    rows[i][k-'1'] = 1;
+                    blocks[i / 3 + j / 3 * 3][k-'1'] = 1;
+                    if (j < 8) {if(dfs(board, i, j+1, cols, rows, blocks)) {return true;}}
+                    else {if(dfs(board, i+1, 0, cols, rows, blocks)){return true;}}
+                    // backtracking: unfill and unchange
+                    board[i][j] = '.';
+                    cols[j][k-'1'] = 0;
+                    rows[i][k-'1'] = 0;
+                    blocks[i / 3 + j / 3 * 3][k-'1'] = 0;
+                }
+            }
+            
+        }
+        return false;
+    }
+    
+    
+    private boolean isValid(int i, int j, int num, int[][] cols, int[][] rows, int[][] blocks) {
+        return cols[j][num] != 1 && rows[i][num] != 1 && blocks[i/3 + j /3 * 3][num] != 1;
+    }
+}
+
+
+
+
+
+// https://leetcode.com/problems/sudoku-solver/discuss/15903/Two-very-Simple-and-Neat-Java-DFSBacktracking-solutions
+// shows two ways
+
+
+
+
+
+
+
+
+
+
+
+
