3039d32cc27b0d5b5c9865d4b8a8e8a3fa0298ee
Codenotlate <286773696@qq.com>
2021-11-22 09:27:22 -0600

    21/11/22 BFS/DFS Review Medium 200

--- Self/200. Number of Islands.java
+++ Self/200. Number of Islands.java
@@ -115,9 +115,121 @@ class Solution {
 
 
 
+// Phase3 self
+// time o(mn) space O(mn)
+// bfs and dfs can be used interchangably here
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] visited = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1' && visited[i][j] != 1) {
+                    count++;
+                    //dfs(grid, i, j, visited); 
+                    // can also change to bfs(grid, i, j, visited)
+                    bfs(grid, i, j, visited);
+                }
+            }
+        }
+        return count;
+    }
+    
+    private int[][] dirs = new int[][]{{-1, 0},{1, 0}, {0, -1}, {0, 1}};
+    
+    // private void dfs(char[][] grid, int i, int j, int[][] visited) {
+    //     int m = grid.length;
+    //     int n = grid[0].length;
+    //     if (i < 0 || i >= m|| j < 0 || j >= n || visited[i][j] == 1 || grid[i][j] != '1') {return;}
+    //     visited[i][j] = 1;
+    //     for (int[] dir: dirs) {
+    //         int newi = i + dir[0];
+    //         int newj = j + dir[1];
+    //         dfs(grid, newi, newj, visited);
+    //     }
+    // }
+    
+    private void bfs(char[][] grid, int i, int j, int[][] visited) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{i, j});
+        visited[i][j] = 1;
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int r = cur[0];
+            int c = cur[1];
+            for (int[] dir: dirs) {
+                int newr = r + dir[0];
+                int newc = c + dir[1];
+                if (newr >= 0 && newr < grid.length && newc >= 0 && newc < grid[0].length && visited[newr][newc] != 1 && grid[newr][newc] == '1') {
+                    visited[newr][newc] = 1;
+                    queue.add(new int[]{newr, newc});
+                }
+            }
+        }
+    }
+}
+
+// phase3: improvement from solution
+// no need to use visited[][], can simply label those visited 1s as 0s in the grid itself.
+// This way, space for DFS is still O(mn), but for BFS the space is the max size of the queue, which is 
 
 
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        for(int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    //dfs(grid, i, j); 
+                    // can also change to bfs(grid, i, j, visited)
+                    bfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+    
+    private int[][] dirs = new int[][]{{-1, 0},{1, 0}, {0, -1}, {0, 1}};
+    
+    // private void dfs(char[][] grid, int i, int j) {
+    //     int m = grid.length;
+    //     int n = grid[0].length;
+    //     if (i < 0 || i >= m|| j < 0 || j >= n || grid[i][j] != '1') {return;}
+    //     grid[i][j] = '0';
+    //     for (int[] dir: dirs) {
+    //         int newi = i + dir[0];
+    //         int newj = j + dir[1];
+    //         dfs(grid, newi, newj);
+    //     }
+    // }
+    
+    private void bfs(char[][] grid, int i, int j) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{i, j});
+        grid[i][j] = '0';
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int r = cur[0];
+            int c = cur[1];
+            for (int[] dir: dirs) {
+                int newr = r + dir[0];
+                int newc = c + dir[1];
+                if (newr >= 0 && newr < grid.length && newc >= 0 && newc < grid[0].length && grid[newr][newc] == '1') {
+                    grid[newr][newc] = '0';
+                    queue.add(new int[]{newr, newc});
+                }
+            }
+        }
+    }
+}
 
+
+// can also do unionFind, omit here for now
 
 
 
