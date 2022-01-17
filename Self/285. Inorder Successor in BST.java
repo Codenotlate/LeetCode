e90c38046a908cb4847e0584207a518f0be1d73e
Codenotlate <286773696@qq.com>
2022-01-16 19:59:36 -0600

    22/1/16 Review Medium BST 285

--- Self/285. Inorder Successor in BST.java
+++ Self/285. Inorder Successor in BST.java
@@ -26,3 +26,62 @@ class Solution {
 }
 
 // skipped the traditional way - which iterates the tree inorder and return the successor.
+
+// Phase 3 self
+// traditional inorder way
+// time O(n) space O(n)
+
+// can also do morris to make space O(1)
+
+class Solution {
+    // record prev popped out node
+    // use regular stack for inorder traverse, space O(n)
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode prevPop = null;
+        while (root != null || !stack.isEmpty()) {
+            while(root!= null) {
+                stack.add(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (prevPop == p) {return root;}
+            prevPop = root;
+            root = root.right;
+            
+        }
+        return null;
+    }
+}
+
+
+// since it's a BST, better utilize its characteristic
+// time O(h) space O(1)
+class Solution {
+    // utilize BST
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        TreeNode res = null;
+        while(root != null) {
+            if (root.val > p.val) {
+                res = root;
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        return res;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
