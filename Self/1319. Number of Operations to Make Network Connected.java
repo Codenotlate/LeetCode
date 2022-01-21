b0854ff2b0448dcbae1fc4b045336ade9ae0d125
Codenotlate <286773696@qq.com>
2022-01-21 11:04:30 -0600

    22/1/21 Review Medium String 49 BFS/DFS/UF 1319 Recursion 979

--- Self/1319. Number of Operations to Make Network Connected.java
+++ Self/1319. Number of Operations to Make Network Connected.java
@@ -132,9 +132,111 @@ class Solution {
 // https://leetcode.com/problems/number-of-operations-to-make-network-connected/discuss/477660/Java-Count-number-of-connected-components-Clean-code
 
 
+// Phase 3 self
+class Solution {
+    // we at least need n-1 edges, otherwise return -1 directly.
+    // If we have at least n-1 edges, then it's guaranteed we can connect all computers by moving some edges.
+    // min number of movements should be the number of unconnected groups - 1. Thus we can use bfs or dfs to count the group number
+    public int makeConnected(int n, int[][] connections) {
+        if (n - 1 > connections.length) {return -1;}
+        // build the adjacent matrix
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int[] edge: connections) {
+            graph.putIfAbsent(edge[0], new LinkedList<Integer>());
+            graph.putIfAbsent(edge[1], new LinkedList<Integer>());
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+        
+        // start bfs/dfs, count the group number
+        int[] visited = new int[n];
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if(visited[i] == 0) {
+                traverse(graph, i, visited);
+                count++;
+            }           
+        }
+        
+        return count-1;
+    }
+    
+    
+    private void traverse(Map<Integer, List<Integer>> graph, int i, int[] visited) {
+        // bfs way
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(i);
+        visited[i] = 1;
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            // don't forget this line!!!!
+            if (graph.get(cur) == null) {continue;}
+            for (int next: graph.get(cur)) {
+                if (visited[next] == 0) {
+                    queue.add(next);
+                    visited[next] = 1;
+                }
+            }
+        }
+    }
+
+    private void traverse(Map<Integer, List<Integer>> graph, int i, int[] visited) {
+        // dfs way
+        if (graph.get(i) == null) {return;}
+        for (int next: graph.get(i)) {
+            if (visited[next] == 1) {continue;}
+            visited[next] = 1;
+            traverse(graph, next, visited);
+        }
+        
+    }
+}
 
 
-
+// M2: UF way: similar as above UF M2
+class Solution {
+    // we at least need n-1 edges, otherwise return -1 directly.
+    // If we have at least n-1 edges, then it's guaranteed we can connect all computers by moving some edges.
+    // min number of movements should be the number of unconnected groups - 1. Thus we can use bfs or dfs to count the group number.
+    // Another way is to use UF to get the group number. time all O(V+E)
+    public int makeConnected(int n, int[][] connections) {
+        if (n - 1 > connections.length) {return -1;}
+        
+        int[] parent = new int[n];
+        int[] rank = new int[n];
+        for (int i = 0; i < n; i++) {parent[i] = i;}
+        
+        for (int[] edge: connections) {
+            union(parent, rank, edge[0], edge[1]);
+        }
+        
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == i) {count++;}
+        }
+        return count-1;
+    }
+    
+    
+    private void union(int[] parent, int[] rank, int p, int q) {
+        int rootp = find(parent, p);
+        int rootq = find(parent, q);
+        if (rank[rootp] > rank[rootq]) {
+            parent[rootq] = rootp;
+        } else {
+            parent[rootp] = rootq;
+            if (rank[rootp] == rank[rootq]) {rank[rootq]++;}
+        }
+    }
+    
+    private int find(int[] parent, int p) {
+        while (parent[p] != p) {
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        }
+        return p;
+    }
+}
 
 
 
--- Self/979. Distribute Coins in Binary Tree.java
+++ Self/979. Distribute Coins in Binary Tree.java
@@ -32,3 +32,31 @@ class Solution {
     	return left + right + root.val - 1;
     }
 }
+
+
+
+// Phase3 self
+class Solution {
+    // The key idea is recursion and get 2 results from bottom-up. For root node, we want to know the movement number of both left subtree and rightsubtree. But the recursion on left/right subtree requires there's no excess/deficit of coins in that subtree. Thus we also need to know the excess and deficit condition from both subtrees, and add the extra number the movements needed to make root node has excess = 0 as well as both subtrees' excess == 0.
+    // That's why we want two info returned by recursion
+    public int distributeCoins(TreeNode root) {
+        int[] res = helper(root);
+        // res[0] records movement number, res[1] records excess/deficit number of coins, which should be zero for root node.
+        return res[0];
+    }
+    
+    
+    private int[] helper(TreeNode root) {
+        if (root == null) {return new int[]{0, 0};}
+        int rootexcess = root.val - 1;
+        int[] left = helper(root.left);
+        int[] right = helper(root.right);
+        int leftmove = left[0];
+        int rightmove = right[0];
+        int leftexcess = left[1];
+        int rightexcess = right[1];
+        rootexcess += leftexcess + rightexcess;
+        int rootmove = Math.abs(leftexcess) + Math.abs(rightexcess) + leftmove + rightmove;
+        return new int[]{rootmove, rootexcess};
+    }
+}
