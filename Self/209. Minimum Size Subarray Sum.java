0ec09d93b3527d99f8130a063423ca4097d25a79
Codenotlate <286773696@qq.com>
2022-01-25 21:47:31 -0600

    22/1/25 Review Medium dp 209 BFS/DFS 934 slidingwindow/BS 209

--- Self/209. Minimum Size Subarray Sum.java
+++ Self/209. Minimum Size Subarray Sum.java
@@ -74,6 +74,60 @@ https://leetcode.com/problems/minimum-window-substring/discuss/26808/Here-is-a-1
 
 
 
+// Review self: similar idea as above M2 but code less clean
+class Solution {
+    // initial thought: sliding window, find first window with sum >= target, then remove the left side of the window as long as the window sum >= target, record the min length along the way.Until the right side of the window reaches the end of the array
+    // time: O(n) since each element in nums being processed at most twice(add in/ remove from the window) space O(1)
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0;
+        int right = 0;
+        int minLen = target;
+        // set the first window
+        int subsum = 0;
+        while(right < nums.length) {
+            subsum += nums[right];
+            if (subsum >= target) {break;}
+            right++;
+        }
+        if (right == nums.length) {return 0;}
+        minLen = Math.min(minLen, right-left+1);
+        while(right < nums.length) {
+            while(left <= right && subsum - nums[left] >= target) {subsum -= nums[left]; left++;}
+            if (left == right) {return 1;}
+            minLen= Math.min(minLen, right-left+1);
+            right++;
+            if(right < nums.length) {subsum += nums[right];}
+                        
+        }
+        return minLen;
+        
+    }
+}
+
+
+
+class Solution {
+    // above way optimized. slightly different moving window rule: first find the window, then move the left side to update the minLen until the window sum is < target, move right side to find the window again and repeat the process.
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0;
+        int right = 0;
+        int minLen = target;
+        // set the first window
+        int subsum = 0;
+        while(right < nums.length) {
+            subsum += nums[right];
+            while (subsum >= target) {
+                minLen = Math.min(minLen, right-left+1);
+                subsum -= nums[left];
+                left++;
+            }
+            right++;
+        }
+        return minLen == target ? 0 : minLen;
+        
+    }
+}
+
 
 
 
--- Self/309. Best Time to Buy and Sell Stock with Cooldown.java
+++ Self/309. Best Time to Buy and Sell Stock with Cooldown.java
@@ -60,3 +60,46 @@ class Solution {
         return dp_i0;   
     }
 } 
+
+
+
+
+// Review self: step by step
+// M1: naive dp way with exact formula implemented. without space optimization. space O(n)  time O(n)
+class Solution {
+    // diff status: at ith day, can choose to buy/sell/do nothing, which is dependent on last day stock owning condition(own or not own), and will impact the next day
+    // dp[i][j] represents the profit on day i with stock owning status j
+    // dp[i][0] = max(dp[i-1][1] + price[i], dp[i-1][0])
+    // Originally, dp[i][1] = max(dp[i-1][0] - price[i], dp[i-1][1]) since cooldown onew day after sell, thus dp[i-1][0] in the above second formula meanings we want to buy at day i, meaning we can't sell at day i-1. Thus that dp[i-1][0] = dp[i-2][0]. Thus: 
+    // dp[i][1] = max(dp[i-2][0]-price[i], dp[i-1][1]).
+    // init: dp[0][0] = 0; dp[0][1] = -inf
+    public int maxProfit(int[] prices) {
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i-1][1] + prices[i], dp[i-1][0]);
+            if (i == 1) {dp[i][1] = Math.max(-prices[i], dp[i-1][1]);}
+            else {dp[i][1] = Math.max(dp[i-2][0] - prices[i], dp[i-1][1]);}
+        }
+        return dp[prices.length-1][0];
+    }
+}
+
+
+// M2: same idea as M1, but optimize with O(1) space.
+class Solution {
+    public int maxProfit(int[] prices) {
+        int dp_previ_0 = 0; // dp[-2][0]
+        int dp_i_0 = 0; //dp[-1][0]
+        int dp_i_1 = Integer.MIN_VALUE; // dp[-1][1]
+        // loop now starts at i = 0
+        for (int p: prices) {
+            int temp = dp_i_0;
+            dp_i_0 = Math.max(dp_i_1 + p, dp_i_0);
+            dp_i_1 = Math.max(dp_previ_0 - p, dp_i_1);
+            dp_previ_0 = temp;
+        }
+        return dp_i_0;
+    }
+}
