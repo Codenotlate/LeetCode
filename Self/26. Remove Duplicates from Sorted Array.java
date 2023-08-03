c30f5429558fafc85cea95224303526b3c018a5f
Codenotlate <286773696@qq.com>
2023-08-02 21:24:19 -0500

    23/8/1 Easy 26

--- Self/26. Remove Duplicates from Sorted Array.java
+++ Self/26. Remove Duplicates from Sorted Array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // similar to 26, again use two pointers
+        // similar to 27, again use two pointers
         if (nums.length <= 1) {return nums.length;}
         int s = 0;
         int f = 1;
@@ -12,5 +12,23 @@ class Solution {
         	f++;
         }
         return s + 1;
+    }
+}
+
+
+
+// 23/8/1 review
+// since now we need to keep relative order, we can no longer use the way in 27 to swap with the right side
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int resEnd = 0;
+        int cur = 0;;
+        while (cur < nums.length) {
+            if (cur == 0 || nums[cur] != nums[cur-1]){
+                nums[resEnd++] = nums[cur];
+            } 
+            cur++;
+        }
+        return resEnd;
     }
 }
